32e7199d5c251e2fa4d383ee5fb3c099e9552ac6
dheim123 <dheim@midland.k12.ia.us>
2026-04-02 12:04:34 -0500

    Refactor ShoppingCart to use Logger dependency

--- ShoppingCart.cs
+++ ShoppingCart.cs
@@ -1,13 +1,19 @@
 namespace knightmoves;
 
-public class ShoppingCart : Logger{
-    public List<string> items {get;}
+public class ShoppingCart 
+{
+    public List<string> Items { get; }
+    private readonly Logger _logger;
 
-    public ShoppingCart(){
-        items = new List<string>();
+    public ShoppingCart(Logger logger)
+    {
+        Items = new List<string>();
+        _logger = logger;
     }
-    public void AddItem(string item){
-        items.Add(item);
-        Log($"Added item '{item}' to cart");
+
+    public void AddItem(string item)
+    {
+        Items.Add(item);
+        _logger.Log($"Added item '{item}' to cart");
     }
 }
