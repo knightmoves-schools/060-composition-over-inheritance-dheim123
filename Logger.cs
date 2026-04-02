d04d4e972407a1bd20071dbbb08367f5377fa775
dheim123 <dheim@midland.k12.ia.us>
2026-04-02 17:10:05 +0000

    1

--- Logger.cs
+++ Logger.cs
@@ -3,21 +3,14 @@ namespace knightmoves;
 public class Logger
 {
     public List<string> Messages { get; }
-    private readonly Logger _parentLogger;
 
     public Logger()
     {
         Messages = new List<string>();
     }
 
-    public Logger(Logger parentLogger) :封装 this()
-    {
-        _parentLogger = parentLogger;
-    }
-
     public void Log(string message)
     {
         Messages.Add(message);
-        _parentLogger?.Log(message);
     }
 }
